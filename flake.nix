182bb3a9ecb199e711a7bfd5e79a8939ef7b177f
Mihai Fufezan <mihai@fufexan.net>
2025-08-21 21:03:56 +0300

    flake: init
    Adds a Nix flake which contains remark with plugins as a Nix package,
    along with a dev shell.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Hyprland wiki";
+
+  outputs = {
+    nixpkgs,
+    systems,
+    self,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system: nixpkgs.legacyPackages.${system});
+  in {
+    packages = eachSystem (system: {
+      hyprland-wiki-lint = pkgsFor.${system}.callPackage ./nix {};
+      default = self.packages.${system}.hyprland-wiki-lint;
+    });
+
+    devShells = eachSystem (system: {
+      default = pkgsFor.${system}.mkShell {
+        packages = with pkgsFor.${system}; [
+          nodejs_22
+          pnpm
+          self.packages.${system}.default
+        ];
+        name = "hyprland-wiki";
+      };
+    });
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # <https://github.com/nix-systems/nix-systems>
+    systems.url = "github:nix-systems/default-linux";
+  };
+}
